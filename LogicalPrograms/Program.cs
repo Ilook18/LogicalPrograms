06b7fda9975e5c4ce84951bfe62946a31e6d004b
“Enter <anirudhanand984@gmail.com>
2021-11-14 21:32:43 +0530

    [Anirudh] add : created Fibonacci series Program

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -6,7 +6,22 @@ namespace LogicalPrograms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the logical Programs");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter the program number which is to be executed \n 1. Fibonacci Series \n 2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FibonacciSeries series = new FibonacciSeries();
+                        series.Fibonacci();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
