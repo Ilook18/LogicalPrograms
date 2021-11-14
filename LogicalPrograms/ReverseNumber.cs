7daddf8ff546e9b65cb2eca5820fbda1151b942f
“Enter <anirudhanand984@gmail.com>
2021-11-14 22:03:09 +0530

    [Anirudh] add : created ReverseNumber Program

---
+++ LogicalPrograms/ReverseNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalPrograms
+{
+    public class ReverseNumber
+    {
+        public void Reverse()
+        {
+            Console.WriteLine("Enter a Number : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int rev = 0, rem;
+            while (num > 0)
+            {
+                rem = num % 10;
+                rev = rev * 10 + rem;
+                num = num / 10;
+            }
+            Console.WriteLine("The Reversed num is : " + rev);
+        }
+    }
+}
